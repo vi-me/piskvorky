4eda676d54f1ba421682cca295e74d40c4f580fd
Viktorie Melichová <viktoriemelichova@gmail.com>
2021-04-18 21:46:45 +0200

    lehce změněná struktura tlačítek v game-page

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict';
 
 let whoseTurn = 'circle';
+let cellsWithCircles = document.querySelector('.cell--circle');
+let cellsWithCrosses = document.querySelector('.cell--cross');
 
 let whoseTurnIndicatorIcon = document.querySelector('.game-page__player-icon');
 
 const makeATurn = (event) => {
   if (whoseTurn === 'circle') {
     event.target.classList.add('cell--circle');
-    whoseTurn = 'cross';
+    event.target.innerHTML = `<img class="img-in-cell"  src="images/circle.svg" alt="Cell taken by Player O"/>`;
     whoseTurnIndicatorIcon.src = 'images/cross.svg';
+    whoseTurn = 'cross';
   } else {
     event.target.classList.add('cell--cross');
+    event.target.innerHTML = `<img class="img-in-cell" src="images/cross.svg" alt="Cell taken by Player X"/>`;
     whoseTurn = 'circle';
     whoseTurnIndicatorIcon.src = 'images/circle.svg';
   }
