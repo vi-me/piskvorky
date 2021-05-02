14e8369a965411005a7c24d894a5cb1b5254fc32
Viktorie Melichová <viktoriemelichova@gmail.com>
2021-05-02 15:57:09 +0200

    It's in ENG now, the logic behind the diagonals is fixed + nor changes in timer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let whoseTurnIndicatorIcon = document.querySelector('.game-page__player-icon');
 
 const cells = document.querySelectorAll('.cell');
 
+// FUNCTION TO MAKE A TURN (draw symbols, change turns, check wins etc.)
 const makeATurn = (event) => {
   if (whoseTurn === 'circle') {
     event.target.classList.add('cell--circle', 'cell--taken');
@@ -15,11 +16,12 @@ const makeATurn = (event) => {
 
     if (isWinningMove(event.target)) {
       setTimeout(function () {
+        // the timeout is here because it'd end the game too early otherwise (the last symbol would not show)
         let r = confirm('Vyhrál hráč s kolečky. Spustit novou hru?');
         if (r) {
           location.reload();
         }
-      }, 380);
+      }, 350);
     }
     console.log('Hráč "O" ukončil svůj tah. Na tahu je hráč "X"');
   } else {
@@ -32,11 +34,12 @@ const makeATurn = (event) => {
 
     if (isWinningMove(event.target)) {
       setTimeout(function () {
+        // the timeout is here because it'd end the game too early otherwise (the last symbol would not show)
         let r = confirm('Vyhrál hráč s křížky. Spustit novou hru?');
         if (r) {
           location.reload();
         }
-      }, 380);
+      }, 350);
     }
     console.log('Hráč "X" ukončil svůj tah. Na tahu je hráč "O"');
   }
@@ -46,6 +49,7 @@ for (let i = 0; i < cells.length; i += 1) {
   cells[i].addEventListener('click', makeATurn);
 }
 
+// FUNCTION TO READ THE SYMBOL FROM A TAKEN CELL
 const getSymbol = (cell) => {
   if (cell.classList.contains('cell--circle')) {
     return 'circle';
@@ -56,8 +60,10 @@ const getSymbol = (cell) => {
 
 const playingBoardSize = 10;
 
+// FUNCTION TO GET THE CELL FROM COORDINATES
 const getCell = (row, column) => cells[row * playingBoardSize + column];
 
+// FUNCTION TO GET THE COORDINATES OF A CELL
 const getPosition = (cell) => {
   let cellIndex = 0;
   while (cellIndex < cells.length && cell !== cells[cellIndex]) {
@@ -70,8 +76,9 @@ const getPosition = (cell) => {
   };
 };
 
-const symbolsToWin = 5;
+const symbolsToWin = 5; // how many symbols are needet to win
 
+// FUNCTION TO CHECK IF IT IS A WINNING MOVE
 const isWinningMove = (cell) => {
   const origin = getPosition(cell);
   const symbol = getSymbol(cell);
@@ -79,19 +86,18 @@ const isWinningMove = (cell) => {
   let i;
   let j;
 
-  //////////VERTIKALY A HORIZONTALY
+  ////// Checking HORIZONTALLY
+  let inRow = 1; // 1 for cell selected
+  let inColumn = 1; // 1 for cell selected
 
-  let inRow = 1; // Jednička pro právě vybrané políčko
-  let inColumn = 1; // Jednička pro právě vybrané políčko
-
-  // Koukni doleva
+  ///////// to the left
   i = origin.column;
   while (i > 0 && symbol === getSymbol(getCell(origin.row, i - 1))) {
     inRow++;
     i--;
   }
 
-  // Koukni doprava
+  ///////// to the right
   i = origin.column;
   while (
     i < playingBoardSize - 1 &&
@@ -101,18 +107,21 @@ const isWinningMove = (cell) => {
     i++;
   }
 
+  ///////// Validate if it is a winning move (horizontally)
   if (inRow >= symbolsToWin) {
     return true;
   }
 
-  // Koukni nahoru
+  ////// Checking VERTICALLY
+
+  ///////// up
   i = origin.row;
   while (i > 0 && symbol === getSymbol(getCell(i - 1, origin.column))) {
     inColumn++;
     i--;
   }
 
-  // Koukni dolu
+  ///////// down
   i = origin.row;
   while (
     i < playingBoardSize - 1 &&
@@ -122,25 +131,16 @@ const isWinningMove = (cell) => {
     i++;
   }
 
+  ///////// Validate if it is a winning move (vertically)
   if (inColumn >= symbolsToWin) {
     return true;
   }
 
-  //////////DIAGONALY
+  ////// Checking DIAGONALLY - down to the LEFT + up to the RIGHT
 
-  let inDiagonalLeft = 1; // Jednička pro právě vybrané políčko
-  let inDiagonalRight = 1; // Jednička pro právě vybrané políčko
+  let inDiagonalDownLeftUpRight = 1; // 1 for cell selected
 
-  // Koukni nahoru doleva
-  i = origin.row;
-  j = origin.column;
-  while (i > 0 && j > 0 && symbol === getSymbol(getCell(i - 1, j - 1))) {
-    inDiagonalLeft++;
-    i--;
-    j--;
-  }
-
-  // Koukni dolů doleva
+  ///////// down to the LEFT
   i = origin.row;
   j = origin.column;
   while (
@@ -148,16 +148,12 @@ const isWinningMove = (cell) => {
     j > 0 &&
     symbol === getSymbol(getCell(i + 1, j - 1))
   ) {
-    inDiagonalLeft++;
+    inDiagonalDownLeftUpRight++;
     i++;
     j--;
   }
 
-  if (inDiagonalLeft >= symbolsToWin) {
-    return true;
-  }
-
-  // Koukni nahoru doprava
+  ///////// up to the RIGHT
   i = origin.row;
   j = origin.column;
   while (
@@ -165,12 +161,20 @@ const isWinningMove = (cell) => {
     j < playingBoardSize - 1 &&
     symbol === getSymbol(getCell(i - 1, j + 1))
   ) {
-    inDiagonalRight++;
+    inDiagonalDownLeftUpRight++;
     i--;
     j++;
   }
 
-  // Koukni dolů doprava
+  ///////// Validate if it is a winning move (down to the LEFT + up to the RIGHT)
+  if (inDiagonalDownLeftUpRight >= symbolsToWin) {
+    return true;
+  }
+
+  ////// Checking DIAGONALLY - down to the RIGHT + up to the LEFT
+  let inDiagonalDownRightUpLeft = 1; // 1 for cell selected
+
+  ///////// down to the RIGHT
   i = origin.row;
   j = origin.column;
   while (
@@ -178,12 +182,22 @@ const isWinningMove = (cell) => {
     j < playingBoardSize - 1 &&
     symbol === getSymbol(getCell(i + 1, j + 1))
   ) {
-    inDiagonalRight++;
+    inDiagonalDownRightUpLeft++;
     i++;
     j++;
   }
 
-  if (inDiagonalRight >= symbolsToWin) {
+  ///////// up to the LEFT
+  i = origin.row;
+  j = origin.column;
+  while (i > 0 && j > 0 && symbol === getSymbol(getCell(i - 1, j - 1))) {
+    inDiagonalDownRightUpLeft++;
+    i--;
+    j--;
+  }
+
+  ///////// Validate if it is a winning move (down to the RIGHT + up to the LEFT)
+  if (inDiagonalDownRightUpLeft >= symbolsToWin) {
     return true;
   }
 
